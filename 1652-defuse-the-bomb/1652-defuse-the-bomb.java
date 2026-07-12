7b4660dd0313f5a2e1bdc1516c5729246e10099e
Praveen292 <praveenkumar29.ms@gmail.com>
2026-07-12 20:10:27 +0530

    Time: 3 ms (17.25%), Space: 43.9 MB (30.05%) - LeetHub

---
+++ 1652-defuse-the-bomb/1652-defuse-the-bomb.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int[] decrypt(int[] code, int k) {
+        int n = code.length;
+        int[] res = new int[n];
+        if(k==0){
+            return res;
+        }
+        if(k>0){
+            for(int i=0;i<n;i++){
+                int sum = 0;
+                for(int j=1;j<=k;j++){
+                    int index = (i+j)%n;
+                    sum+=code[index];
+                }
+                res[i] = sum;
+             }
+        }
+        else{
+            for(int i=0;i<n;i++){
+                int sum = 0;
+                for(int j=1;j<=-k;j++){
+                    int index = (i-j+n)%n;
+                    sum += code[index];
+                }
+                res[i] = sum;
+            }
+        }
+        return res;
+        
+    }
+}
